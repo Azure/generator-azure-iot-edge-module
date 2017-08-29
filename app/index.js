ae6520fb750ceae1baa74730999dd5f820e1fc9d
Qingqing Sun <qisun@microsoft.com>
2017-08-29 14:42:05 +0800

    use when condition for module name, architectures and test prompt. Add test.

--- app/index.js
+++ app/index.js
@@ -11,77 +11,83 @@ module.exports = class extends Generator {
     this.log(yosay('Hey You\r\nWelcome to Azure IoT Edge Module Generator'));
 
     return this.prompt([{
-      type: 'list',
-      name: 'moduleType',
-      choices: [
-        "custom module", "deployment file", "routes file"
-      ],
-      message: "Which one would you like to create?"
-    }]).then((answers) => {
-      this.moduleType = answers.moduleType;
-      if (this.moduleType === 'custom module') {
-        this.prompt([{
-            type: 'input',
-            name: 'name',
-            default: 'AzureIoTEdgeModule',
-            message: 'What\'s the name of your module'
+        type: 'list',
+        name: 'moduleType',
+        choices: [
+          "custom module", "deployment file", "routes file"
+        ],
+        message: "Which one would you like to create?"
+      },
+      {
+        when: function (props) {
+          return props.moduleType === 'custom module';
+        },
+        type: 'input',
+        name: 'name',
+        default: 'AzureIoTEdgeModule',
+        message: 'What\'s the name of your module'
+      },
+      {
+        when: function (props) {
+          return props.moduleType === 'custom module';
+        },
+        type: 'checkbox',
+        name: 'architectures',
+        message: 'Select the architecture(s) you want to support, your choice will help generate the corresponding dockerfile(s).',
+        choices: [{
+            name: "windows-x64"
           },
           {
-            type: 'checkbox',
-            name: 'architectures',
-            message: 'Select the architecture(s) you want to support, your choice will help generate the corresponding dockerfile(s).',
-            choices: [{
-                name: "windows-x64"
-              },
-              {
-                name: "linux-x64"
-              },
-              {
-                name: "windows-x86"
-              },
-              {
-                name: "linux-x86"
-              },
-              {
-                name: "linux-arm32"
-              },
-            ],
-            validate: function (answer) {
-              if (answer.length < 1) {
-                return 'You must select at least one architecture.';
-              }
-              return true;
-            }
+            name: "linux-x64"
           },
           {
-            type: 'confirm',
-            name: 'test',
-            message: 'Would you like to add unit test?'
+            name: "windows-x86"
+          },
+          {
+            name: "linux-x86"
+          },
+          {
+            name: "linux-arm32"
+          },
+        ],
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return 'You must select at least one architecture.';
           }
-        ]).then((props) => {
-          this.log('Creating custom module...');
-          if (props.test) {
-            fs.mkdir(this.destinationPath(props.name + 'Test'));
-          }
-          this.fs.copyTpl(this.templatePath('app.js'), this.destinationPath(props.name + '/app.js'));
-          this.fs.copyTpl(this.templatePath('.npmrc'), this.destinationPath(props.name + '/.npmrc'));
-          this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath(props.name + '/package.json'), {
-            ModuleName: props.name
-          });
-          this.fs.copyTpl(this.templatePath('package-lock.json'), this.destinationPath(props.name + '/package-lock.json'), {
-            ModuleName: props.name
-          });
-          props.architectures.forEach((architecture, index) => {
-            this.fs.copyTpl(this.templatePath('Dockerfile'), this.destinationPath(props.name + '/Docker/' + architecture + '/Dockerfile'));
-          });
+          return true;
+        }
+      },
+      {
+        when: function (props) {
+          return props.moduleType === 'custom module';
+        },
+        type: 'confirm',
+        name: 'test',
+        message: 'Would you like to add unit test?'
+      }
+    ]).then((answers) => {
+      if (answers.moduleType === 'custom module') {
+        this.log('Creating files...');
+        if (answers.test) {
+          fs.mkdir(this.destinationPath(answers.name + 'Test'));
+          fs.writeFileSync('test.js','')
+        }
+        this.fs.copyTpl(this.templatePath('app.js'), this.destinationPath(answers.name + '/app.js'));
+        this.fs.copyTpl(this.templatePath('.npmrc'), this.destinationPath(answers.name + '/.npmrc'));
+        this.fs.copyTpl(this.templatePath('package.json'), this.destinationPath(answers.name + '/package.json'), {
+          ModuleName: answers.name
         });
-      } else if (this.moduleType === 'deployment file') {
-        this.log('Creating deployment file...');
+        this.fs.copyTpl(this.templatePath('package-lock.json'), this.destinationPath(answers.name + '/package-lock.json'), {
+          ModuleName: answers.name
+        });
+        answers.architectures.forEach((architecture, index) => {
+          this.fs.copyTpl(this.templatePath('Dockerfile'), this.destinationPath(answers.name + '/Docker/' + architecture + '/Dockerfile'));
+        });
+      } else if (answers.moduleType === 'deployment file') {
         this.fs.copyTpl(this.templatePath('deployment.json'), this.destinationPath('deployment.json'));
-      } else if (this.moduleType === 'routes file') {
-        this.log('Creating routes file...');
+      } else if (answers.moduleType === 'routes file') {
         this.fs.copyTpl(this.templatePath('routes.json'), this.destinationPath('routes.json'));
       }
     });
   }
-};
+}
---
+++ test/test-app.js
@@ -0,0 +1,43 @@
+"user strict";
+
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+var path = require('path');
+
+describe('generator-azure-iot-edge-module: app', function () {
+    it('should generate all custom module files', function () {
+        return helpers.run(path.join(__dirname, '../app'))
+            .withPrompts({
+                moduleType: 'custom module',
+                name: 'TestModule',
+                architectures: ['windows-x64'],
+                test: false
+            })
+            .then(() => {
+                assert.file(['TestModule/app.js', 'TestModule/package.json', 'TestModule/package-lock.json', 'TestModule/.npmrc', 'TestModule/Docker/windows-x64/Dockerfile']);
+                assert.noFile('Test/test.js');
+                assert.noFile('deployment.json');
+                assert.noFile('routes.json');
+            });
+    });
+
+    it('should generate deployment.json file when select the module type', function () {
+        return helpers.run(path.join(__dirname, '../app'))
+            .withPrompts({
+                moduleType: 'deployment file'
+            })
+            .then(() => {
+                assert.file('deployment.json');
+            });
+    });
+
+    it('should generate routes.json file when select the module type', function () {
+        return helpers.run(path.join(__dirname, '../app'))
+            .withPrompts({
+                moduleType: 'routes file'
+            })
+            .then(() => {
+                assert.file('routes.json');
+            });
+    });
+});
