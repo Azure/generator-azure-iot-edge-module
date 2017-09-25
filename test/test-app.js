8c6f1116340fe179243d729a6d5f8fe4a8310b86
Summer <victoriasqq@gmail.com>
2017-09-25 17:39:11 +0800

    update tests

--- test/test-app.js
+++ test/test-app.js
@@ -11,13 +11,30 @@ describe('generator-azure-iot-edge-module: app', function () {
                 moduleType: 'custom module',
                 name: 'TestModule',
                 architectures: ['linux-x64'],
+                restore: false,
                 test: false
             })
             .then(() => {
-                assert.file(['TestModule/app.js', 'TestModule/package.json', 'TestModule/package-lock.json', 'TestModule/.npmrc', 'TestModule/Docker/linux-x64/Dockerfile']);
-                assert.noFile('Test/test.js');
-                assert.noFile('deployment.json');
-                assert.noFile('routes.json');
+                assert.file(['TestModule/app.js', 'TestModule/package.json', 'TestModule/package-lock.json', 'TestModule/.gitignore', 'TestModule/.npmrc', 'TestModule/Docker/linux-x64/Dockerfile']);
+                assert.noFile('TestModule//Test/test.js');
+                assert.noFile('TestModule/deployment.json');
+                assert.noFile('TestModule/routes.json');
+            });
+    });
+
+    it('should generate test files', function () {
+        return helpers.run(path.join(__dirname, '../app'))
+            .withPrompts({
+                moduleType: 'custom module',
+                name: 'TestModule',
+                architectures: ['linux-x64'],
+                restore: false,
+                test: true
+            })
+            .then(() => {
+                assert.file(['TestModule/app.js', 'TestModule/package.json', 'TestModule/package-lock.json', 'TestModule/.gitignore', 'TestModule/.npmrc', 'TestModule/Docker/linux-x64/Dockerfile', 'TestModule//Test/test.js']);
+                assert.noFile('TestModule/deployment.json');
+                assert.noFile('TestModule/routes.json');
             });
     });
 
