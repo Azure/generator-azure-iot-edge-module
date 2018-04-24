bf0902eaaa2379ffa81ddf302f66ea4cf6d6bae1
Ray Fang <fangzh@microsoft.com>
2018-04-24 17:06:48 +0800

    Update template code

--- app/templates/app.js
+++ app/templates/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var Protocol = require('azure-iot-device-mqtt').Mqtt;
+var Transport = require('azure-iot-device-mqtt').Mqtt;
 var Client = require('azure-iot-device').Client;
 var Message = require('azure-iot-device').Message;
 var fs = require('fs');
 
 var connectionString = process.env.EdgeHubConnectionString;
+var caCertFile = process.env.EdgeModuleCACertificateFile;
 
-var client = Client.fromConnectionString(connectionString, Protocol);
+var client = Client.fromConnectionString(connectionString, Transport);
 console.log("Connection String: " + connectionString);
 
 client.on('error', function (err) {
@@ -15,7 +16,7 @@ client.on('error', function (err) {
 });
 
 client.setOptions({
-  ca: fs.readFileSync(process.env.EdgeModuleCACertificateFile).toString('ascii')
+  ca: fs.readFileSync(caCertFile).toString('ascii')
 }, function (err) {
   if (err) {
     console.log('error:' + err);
@@ -29,21 +30,25 @@ client.setOptions({
 
         // Act on input messages to the module.
         client.on('inputMessage', function (inputName, msg) {
-
-          client.complete(msg, printResultFor('Receiving message:'));
-
-          if (inputName === 'input1') {
-            var message = msg.getBytes().toString('utf8');
-            if (message) {
-              var pipeMessage = new Message(message);
-              client.sendOutputEvent('output1', pipeMessage, printResultFor('Sending received message'));
-            }
-          }
+          pipeMessage(inputName, msg);
         });
       }
     });
   }
 });
+
+// This function just pipes the messages without any change.
+function pipeMessage(inputName, msg) {
+  client.complete(msg, printResultFor('Receiving message:'));
+
+  if (inputName === 'input1') {
+    var message = msg.getBytes().toString('utf8');
+    if (message) {
+      var outputMsg = new Message(message);
+      client.sendOutputEvent('output1', outputMsg, printResultFor('Sending received message'));
+    }
+  }
+}
 
 // Helper function to print results in the console
 function printResultFor(op) {
